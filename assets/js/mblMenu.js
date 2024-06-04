3d4b5019e655de195a02c5119ad8a51f59a7d4bf
Jose Antonio Sánchez <crimson3d@gmail.com>
2024-06-04 16:10:36 +0200

    cv update

--- assets/js/mblMenu.js
+++ assets/js/mblMenu.js
@@ -5,7 +5,7 @@ for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
         let panel = this.nextElementSibling;
-        if (panel.style.display === "block" || panel.style.display === "grid") {
+        if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
             panel.style.display = "block";
--- assets/js/script.js
+++ assets/js/script.js
@@ -43,12 +43,12 @@ async function fetchData() {
         if (cvData?.skills) {
             const skillsContainer = document.getElementById("skills");
             if (skillsContainer) {
-                let skillsHTML = '<h3 class="section__title accordion">HABILIDADES</h3>';
+                let skillsHTML = '<h3 class="section__title">HABILIDADES</h3>';
 
                 // Recorre cada conjunto de habilidades en el array skills
                 cvData.skills.forEach((skillSet) => {
                     // Añade un bloque de habilidades sin tener en cuenta name ni level
-                    skillsHTML += `<div class="section__set panel">`;
+                    skillsHTML += `<div class="section__set">`;
 
                     // Recorre el array de keywords y añade cada uno al HTML
                     skillSet.keywords.forEach(keyword => {
@@ -65,10 +65,10 @@ async function fetchData() {
         if (cvData?.languages) {
             const languagesContainer = document.getElementById("languages");
             if (languagesContainer) {
-                let languagesHTML = '<h3 class="section__title accordion">IDIOMAS</h3>';
+                let languagesHTML = '<h3 class="section__title">IDIOMAS</h3>';
 
                 cvData.languages.forEach((languageSet) => {
-                    languagesHTML += `<p class="section__text panel">${languageSet.language}: ${languageSet.fluency}</p>`;
+                    languagesHTML += `<p class="section__text">${languageSet.language}: ${languageSet.fluency}</p>`;
                     languagesContainer.innerHTML = languagesHTML;
                 });
             }
@@ -89,10 +89,10 @@ async function fetchData() {
         if (cvData?.education) {
             const educationContainer = document.getElementById("study");
             if (educationContainer) {
-                let educationHTML = '<div class="section__top"><div class="top__icon"><img src="./assets/images/education-svgrepo-com.svg" alt="certificates-icon" class="icon" /></div><h3 class="section__title accordion">FORMACIÓN</h3></div>';
+                let educationHTML = '<div class="section__top"><div class="top__icon"><img src="./assets/images/education-svgrepo-com.svg" alt="certificates-icon" class="icon" /></div><h3 class="section__title">FORMACIÓN</h3></div>';
 
                 cvData.education.forEach((educationSet) => {
-                    educationHTML += `<div class="section__text panel">
+                    educationHTML += `<div class="section__text">
                     <div class="section__info">
                         <p class="text__education__area">${educationSet.area}</p>
                         <p class="info__years">${educationSet.year}</p>
@@ -109,11 +109,11 @@ async function fetchData() {
         if (cvData?.certificates) {
             const certificatesContainer = document.getElementById("certificates");
             if (certificatesContainer) {
-                let certificatesHTML = '<div class="section__top"><div class="top__icon"> <img src="./assets/images/star-svgrepo-com.svg" alt="certificates-icon" class="icon" /></div><h3 class="section__title accordion">CERTIFICADOS</h3></div>';
+                let certificatesHTML = '<div class="section__top"><div class="top__icon"> <img src="./assets/images/star-svgrepo-com.svg" alt="certificates-icon" class="icon" /></div><h3 class="section__title">CERTIFICADOS</h3></div>';
 
                 cvData.certificates.forEach((certificatesSet) => {
                     certificatesHTML += `
-                    <div class="section__text panel">
+                    <div class="section__text">
                         <p class="text__certificates__name">${certificatesSet.name}</p>
                         <div class="text__bottom">
                             <p class="bottom__text">${certificatesSet.issuer}</p>
@@ -129,17 +129,17 @@ async function fetchData() {
         if (cvData?.work) {
             const workContainer = document.getElementById("work");
             if (workContainer) {
-                let workHTML = '<div class="section__top"><div class="top__icon"> <img src="./assets/images/investigation-svgrepo-com.svg" alt="work-icon" class="icon" /></div><h3 class="section__title accordion">EXPERIENCIA LABORAL</h3></div>';
+                let workHTML = '<div class="section__top"><div class="top__icon"> <img src="./assets/images/investigation-svgrepo-com.svg" alt="work-icon" class="icon" /></div><h3 class="section__title">EXPERIENCIA LABORAL</h3></div>';
 
                 cvData.work.forEach((workSet) => {
                     workHTML += `
-                    <div class="section__work panel">
-                        <div class="work__text">
+                    <div class="work__card">
+                        <div class="card__text">
                             <p class="text__position">${workSet.position}</p>
                             <p class="text__date">${workSet.startDate} - ${workSet.endDate}</p>
                         </div>
-                        <p class="work__middle">${workSet.name}</p>
-                        <p class="work__bottom">${workSet.summary}</p>
+                        <p class="card__middle">${workSet.name}</p>
+                        <p class="card__bottom">${workSet.summary}</p>
                     </div>`;
 
                     workContainer.innerHTML = workHTML;
